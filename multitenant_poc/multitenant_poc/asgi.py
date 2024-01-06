907c991e38555c913f1a66b315b0e4f388c8e87c
Pablo Pissi <pablopissi@gmail.com>
2024-01-06 00:44:01 -0300

    config db y modelo inicial

---
+++ multitenant_poc/multitenant_poc/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for multitenant_poc project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'multitenant_poc.settings')
+
+application = get_asgi_application()
